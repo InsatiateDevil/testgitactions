019550c34d17dd3f7d4b324b41a8c8d83c927b96
Insatiate Devil <ILIDANUM@yandex.ru>
2025-02-15 20:37:07 +0300

    agdsag

--- config/settings.py
+++ config/settings.py
@@ -141,9 +141,8 @@ TELEGRAM_BOT_TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'static'
-STATICFILES_DIRS = [BASE_DIR / 'staticfiles',]
 
 MEDIA_URL = "/media"
 MEDIA_ROOT = BASE_DIR / "media"
--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from django.contrib import admin
 from django.urls import path, include
+
+from config import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -40,3 +43,6 @@ urlpatterns = [
     ),
     path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
